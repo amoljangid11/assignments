27762952362b992b3353600fc0aa8f9d0c054c65
amol <amoljangid33@gmail.com>
2024-04-18 10:17:58 +0530

    first commit

---
+++ ReactJSProject1/simple-app/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Simple from './components/Simple';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>This is the App Component</h1>
+      <Simple />
+    </div>
+  );
+}
+
+export default App;
